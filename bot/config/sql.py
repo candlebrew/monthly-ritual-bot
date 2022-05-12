05f4c5706092efb4bf81a1b05acf96054f162060
candlebrew <78180295+candlebrew@users.noreply.github.com>
2022-05-12 13:09:36 -0500

    Update sql.py

--- bot/config/sql.py
+++ bot/config/sql.py
@@ -20,3 +20,55 @@ userDataTableSQL = '''CREATE TABLE IF NOT EXISTS user_data (
 ## next_men_start: last_men_start + men_len
 ## men_to_ov_len: last_men_start <> last_ov_start
 ## next_ov_start: last_men_start + men_to_ov_len
+
+## PREFERENCES --------------------------
+prefTableSQL = '''CREATE TABLE IF NOT EXISTS preferences (
+        uid BIGINT,
+        water_alert BOOL DEFAULT FALSE,
+        water_min SMALLINT DEFAULT 0,
+        water_hr SMALLINT DEFAULT 1,
+        med_alert BOOL DEFAULT FALSE,
+        med_frequency TEXT,
+        med_time TIMESTAMPTZ,
+        med_next_alert TIMESTAMPTZ,
+        hrt_alert BOOL DEFAULT FALSE,
+        hrt_frequency TEXT,
+        hrt_time TIMESTAMPTZ,
+        hrt_next_alert TIMESTAMPTZ
+        );'''
+## frequencies: daily/weekly/biweekly/monthly
+
+## SYMPTOMS -----------------------------
+symptomsTableSQL = '''CREATE TABLE IF NOT EXISTS symptoms (
+        uid BIGINT,
+        id SERIAL UNIQUE,
+        time TIMESTAMPTZ,
+        symptom TEXT,
+        rating TEXT
+        );'''
+
+## ENCOUNTERS ---------------------------
+encountersTableSQL = '''CREATE TABLE IF NOT EXISTS encounters (
+        uid BIGINT,
+        id SERIAL UNIQUE,
+        start TIMESTAMPTZ,
+        end TIMESTAMPTZ,
+        length SMALLINT,
+        abnormal BOOL DEFAULT FALSE,
+        rating TEXT,
+        type TEXT
+        );'''
+
+## TERMS --------------------------
+defaultTermsTableSQL = '''CREATE TABLE IF NOT EXISTS default_terms (
+        name TEXT,
+        alias TEXT,
+        type TEXT
+        );'''
+
+customTermsTableSQL = '''CREATE TABLE IF NOT EXISTS custom_terms (
+        uid BIGINT,
+        name TEXT,
+        alias TEXT,
+        type TEXT
+        );'''
